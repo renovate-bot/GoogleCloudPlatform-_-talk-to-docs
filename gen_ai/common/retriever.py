f52249992e5cc3bcc0185e47eeebce46c8d0ea86
Dauren <dauren@google.com>
2024-04-30 20:55:52 +0000

    PR changes

--- gen_ai/common/retriever.py
+++ gen_ai/common/retriever.py
@@ -143,11 +143,13 @@ def retrieve_initial_documents(
                                                         similar_questions_number=similar_questions_number
                                                         )
         Container.logger().info(msg=f"Questions:\n {similar_questions}")
+        questions_for_search = question + "\n" + similar_questions
+        questions_for_search = questions_for_search.split("?")
+        questions_for_search = [x.replace("\n", "").strip() for x in questions_for_search]
+        questions_for_search = [f"{x}?" for x in questions_for_search if x][0:similar_questions_number+1]
+    else:
+        questions_for_search = [question]
 
-    questions_for_search = question + "\n" + similar_questions
-    questions_for_search = questions_for_search.split("?")
-    questions_for_search = [x.replace("\n", "").strip() for x in questions_for_search]
-    questions_for_search = [f"{x}?" for x in questions_for_search if x][0:similar_questions_number+1]
     Container.logger().info(msg=f"Questions after processing:\n {questions_for_search}")
 
     retrieved_docs, post_filtered_docs = perform_retrieve_round(
