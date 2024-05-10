51fb4f1d4023c84dfdfaf7f43cc0656ea79990b4
misha-chertushkin <chertushkin@google.com>
2024-05-10 02:06:02 +0000

    Fix the conftest

--- gen_ai/tests/conftest.py
+++ gen_ai/tests/conftest.py
@@ -6,7 +6,6 @@ from gen_ai import constants
 @pytest.fixture(autouse=True, scope="session")
 def override_processed_files_dir():
     original_dir = constants.PROCESSED_FILES_DIR
-    updated_dir = f'{os.getenv("HOME")}/resources/uhg/main_folder'
-    constants.PROCESSED_FILES_DIR = updated_dir
+    constants.PROCESSED_FILES_DIR = f'{os.getenv("HOME")}/resources/uhg/main_folder'
     yield
     constants.PROCESSED_FILES_DIR = original_dir
