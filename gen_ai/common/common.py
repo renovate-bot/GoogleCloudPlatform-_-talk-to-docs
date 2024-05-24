46df6917a2870dfbfac423461d0d4d4469bda12d
Dauren <dauren@google.com>
2024-05-24 22:01:02 +0000

    llms order change

--- gen_ai/common/common.py
+++ gen_ai/common/common.py
@@ -119,16 +119,16 @@ def get_or_create_model(model_name: str) -> BaseChatModel:
     config = load_yaml(LLM_YAML_FILE)
     temperature = config.get("temperature", 0.001)
     max_output_tokens = config.get("max_output_tokens", 4000)
-    if "gpt-3.5" in model_name or "gpt-4" in model_name:
-        llms[model_name] = ChatOpenAI(model_name=model_name, temperature=temperature)
+    if "unicorn" in model_name or "gemini" in model_name:
+        llms[model_name] = VertexAI(model_name=model_name, temperature=temperature, max_output_tokens=max_output_tokens)
         return llms[model_name]
     elif "chat-bison" in model_name or "text-bison" in model_name:
         llms[model_name] = ChatVertexAI(
             model_name=model_name, temperature=temperature, max_output_tokens=max_output_tokens
         )
         return llms[model_name]
-    elif "unicorn" in model_name or "gemini" in model_name:
-        llms[model_name] = VertexAI(model_name=model_name, temperature=temperature, max_output_tokens=max_output_tokens)
+    elif "gpt-3.5" in model_name or "gpt-4" in model_name:
+        llms[model_name] = ChatOpenAI(model_name=model_name, temperature=temperature)
         return llms[model_name]
     else:
         raise ValueError("Unknown model name")
