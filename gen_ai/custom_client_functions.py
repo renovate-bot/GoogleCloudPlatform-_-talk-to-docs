e610f8bacaf051bbb3ac4fd0d3a5a0aca2e73c0a
Dauren <dauren@google.com>
2024-07-26 23:15:09 +0000

    Merge multiple docs sources and generate single context

--- gen_ai/custom_client_functions.py
+++ gen_ai/custom_client_functions.py
@@ -8,15 +8,21 @@ import copy
 import os
 from collections import defaultdict
 
+from dependency_injector.wiring import inject
 from langchain.schema import Document
 from langchain_community.vectorstores.chroma import Chroma
 
 import gen_ai.common.common as common
+from gen_ai.common.common import TokenCounter, split_large_document, update_used_docs
 from gen_ai.common.document_retriever import SemanticDocumentRetriever
+from gen_ai.common.ioc_container import Container
 from gen_ai.common.measure_utils import trace_on
 from gen_ai.common.storage import Storage
 from gen_ai.deploy.model import QueryState
 
+
+def generate_contexts_from_docs(docs_and_scores: list[Document], query_state: QueryState | None = None) -> list[str]:
+    return default_generate_contexts_from_docs(docs_and_scores, query_state)
 
 def build_doc_title(metadata: dict[str, str]) -> str:
     """Builds a document title using chosen behavior.
@@ -290,3 +296,113 @@ def default_build_doc_title(metadata: dict[str, str]) -> str:
     if metadata.get("symbols"):
         doc_title += metadata["symbols"] + " "
     return doc_title
+
+
+def custom_generate_contexts_from_docs(docs_and_scores: list[Document], query_state: QueryState | None = None) -> list[str]:
+    kc_docs = [x for x in docs_and_scores if x.metadata["data_source"] == "kc"]
+    b360_docs = [x for x in docs_and_scores if x.metadata["data_source"] == "b360"]
+    kc_context = default_generate_contexts_from_docs(kc_docs, query_state)[0]
+    b360_context = default_generate_contexts_from_docs(b360_docs, query_state)[0]
+
+    custom_context = f"""
+    <b360_context>
+        {b360_context}
+    </b360_context>
+    This marks the start of text from kc documents. Remember that these are generic documents that will be overridden by a b360 documents, in case of a conflict re: coverage of a service.
+    <kc_context>
+        {kc_context}
+    </kc_context>
+    """
+    return [custom_context]
+
+
+@inject
+@trace_on("Generating context from documents", measure_time=True)
+def default_generate_contexts_from_docs(docs_and_scores: list[Document], query_state: QueryState | None = None) -> list[str]:
+    """
+    Generates textual contexts from a list of documents, preparing them for input to a language model.
+
+    This function processes each document to extract content up to a specified token limit, organizing this content
+    into manageable sections that fit within the maximum context size for a language model. It handles large documents
+    by splitting them into chunks and maintains a count of used tokens and documents to optimize the subsequent
+    language model processing.
+
+    Args:
+        docs_and_scores (list[Document]): A list of Document objects, each containing metadata and content
+            to be used in generating context. These documents are assumed to be scored and potentially filtered
+            by relevance to the query.
+        query_state (QueryState): The current state of the query, including details like previously used tokens
+            and documents. This state is updated during the function execution to include details about the
+            documents and tokens used in this invocation.
+
+    Returns:
+        list[str]: A list of strings, where each string represents a textual context segment formed from the
+            document content. Each segment is designed to be within the token limits suitable for processing
+            by a language model.
+
+    Raises:
+        ValueError: If any document does not contain the necessary content or metadata for processing.
+
+    Examples:
+        >>> docs = [Document(page_content="Content of the document.",
+        metadata={"section_name": "Section 1", "summary": "Summary of the document.", "relevancy_score": 0.95})]
+        >>> query_state = QueryState(question="What is the purpose of the document?",
+        answer="To provide information.", additional_information_to_retrieve="")
+        >>> contexts = generate_contexts_from_docs(docs, query_state)
+        >>> print(contexts[0])
+        "Content of the document."
+
+    Note:
+        The function modifies the `query_state` object in-place, updating it with details about the tokens and
+        documents used during the context generation process. Ensure that the `query_state` object is appropriately
+        handled to preserve the integrity of the conversation state.
+    """
+    num_docs_used = [0]
+    contexts = ["\n"]
+    token_counts = [0]
+    used_articles = []
+    token_counter: TokenCounter = Container.token_counter()
+    max_context_size = Container.config.get("max_context_size", 1000000)
+
+    for doc in docs_and_scores:
+        filename = doc.metadata["section_name"]
+
+        doc_content = doc.page_content if Container.config.get("use_full_documents", False) else doc.metadata["summary"]
+        doc_tokens = token_counter.get_num_tokens_from_string(doc_content)
+        if doc_tokens > max_context_size:
+            doc_chunks = split_large_document(doc_content, max_context_size)
+        else:
+            doc_chunks = [(doc_content, doc_tokens)]
+
+        for doc_chunk, doc_tokens in doc_chunks:
+
+            if token_counts[-1] + doc_tokens >= max_context_size:
+                token_counts.append(0)
+                contexts.append("\n")
+                num_docs_used.append(0)
+
+            used_articles.append((f"{filename} Context: {len(contexts)}", doc.metadata["relevancy_score"]))
+            token_counts[-1] += doc_tokens
+
+            contexts[-1] += "DOCUMENT TITLE: "
+            contexts[-1] += build_doc_title(doc.metadata) + "\n"
+            contexts[-1] += "DOCUMENT CONTENT: "
+            contexts[-1] += doc_chunk
+            contexts[-1] += "\n" + "-" * 12 + "\n"
+            num_docs_used[-1] += 1
+
+    contexts[-1] += "\n"
+
+    Container.logger().info(msg=f"Docs used: {num_docs_used}, tokens used: {token_counts}")
+
+    if query_state:
+        query_state.input_tokens = token_counts
+        query_state.num_docs_used = num_docs_used
+        query_state.used_articles_with_scores = update_used_docs(used_articles, query_state)
+        Container.logger().info(msg=f"Doc names with relevancy scores: {query_state.used_articles_with_scores}")
+
+    doc_attributes = extract_doc_attributes(docs_and_scores)
+    Container.logger().info(msg=f"Doc attributes: {doc_attributes}")
+    return contexts
+
+
