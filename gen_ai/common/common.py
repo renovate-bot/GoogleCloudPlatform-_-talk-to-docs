cf3cf58759b5aacd9dd652b9a45c1b35a3c6060d
WonderWoman <anovakovska@google.com>
2024-09-18 23:54:24 +0000

    add pre os changes: medicare prompts, seed setup, changes in similar questions

--- gen_ai/common/common.py
+++ gen_ai/common/common.py
@@ -120,15 +120,12 @@ def get_or_create_model(model_name: str) -> BaseChatModel:
     temperature = config.get("temperature", 0.001)
     max_output_tokens = config.get("max_output_tokens", 4000)
     if "gemini" in model_name or "unicorn" in model_name:
-        llms[model_name] = VertexAI(model_name=model_name, temperature=temperature, max_output_tokens=max_output_tokens)
+        llms[model_name] = VertexAI(model_name=model_name, temperature=temperature, max_output_tokens=max_output_tokens, seed=42)
         return llms[model_name]
     elif "chat-bison" in model_name or "text-bison" in model_name:
         llms[model_name] = ChatVertexAI(
             model_name=model_name, temperature=temperature, max_output_tokens=max_output_tokens
         )
-        return llms[model_name]
-    elif "gpt-3.5" in model_name or "gpt-4" in model_name:
-        llms[model_name] = ChatOpenAI(model_name=model_name, temperature=temperature)
         return llms[model_name]
     else:
         raise ValueError("Unknown model name")
