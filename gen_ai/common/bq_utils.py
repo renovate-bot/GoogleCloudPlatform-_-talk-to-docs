1908e456113fa955c19203140c16384e34f647ba
Eva Khmelinskaya <evekhm@gmail.com>
2024-09-30 14:55:50 -0700

    app_version (version.txt)

--- gen_ai/common/bq_utils.py
+++ gen_ai/common/bq_utils.py
@@ -50,6 +50,7 @@ from google.cloud.bigquery.schema import SchemaField
 from gen_ai.common.document_utils import convert_dict_to_relevancies, convert_dict_to_summaries
 from gen_ai.common.ioc_container import Container
 from gen_ai.deploy.model import Conversation, QueryState
+from gen_ai import __version__
 
 
 def create_dataset(
@@ -344,22 +345,32 @@ class BigQueryConverter:
             "relevance_score": [],
             "additional_question": [],
             "plan_and_summaries": [],
-            "original_question": []
+            "original_question": [],
+            "app_version": []
         }
         max_round = len(log_snapshots) - 1
         system_state_id = Container.system_state_id or log_system_status(session_id)
+        app_version = __version__
         for round_number, log_snapshot in enumerate(log_snapshots):
             react_round_number = round_number
             response = query_state.answer or ""
             retrieved_documents_so_far = json.dumps(
                 [
-                    {"original_filepath": x["metadata"].get("original_filepath")}
+                    {
+                        "original_filepath": x["metadata"].get("original_filepath"),
+                        "doc_identifier": x["metadata"].get("doc_identifier"),
+                        "section_name": x["metadata"].get("section_name"),
+                    }
                     for x in log_snapshot["pre_filtered_docs"]
                 ]
             )
             post_filtered_documents_so_far = json.dumps(
                 [
-                    {"original_filepath": x["metadata"].get("original_filepath")}
+                    {
+                        "original_filepath": x["metadata"].get("original_filepath"),
+                        "doc_identifier": x["metadata"].get("doc_identifier"),
+                        "section_name": x["metadata"].get("section_name"),
+                    }
                     for x in log_snapshot["post_filtered_docs"]
                 ]
             )
@@ -411,6 +422,7 @@ class BigQueryConverter:
             data["additional_question"].append(additional_question)
             data["plan_and_summaries"].append(plan_and_summaries)
             data["original_question"].append(query_state.original_question)
+            data["app_version"].append(app_version)
 
         df = pd.DataFrame(data)
         return df
